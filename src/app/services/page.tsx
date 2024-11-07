cff45f1218a7fcb2c604fc67e190f9cd54550578
TN <tn@macbook-pro-tn.home>
2024-11-07 12:16:40 -0800

    services

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,3 +1,56 @@
+"use client"
+import { BsArrowDownRight } from "react-icons/bs";
+import Link from "next/link";
+import { motion } from "framer-motion"
+const services = [
+    {
+        num: "01",
+        title: "Usuwanie korzeni"
+
+    },
+    {
+        num: "02",
+        title: "Prace ogrodowe"
+    },
+    {
+        num: "03",
+        title: "Drenaże, odwodnienia"
+    },
+    {
+        num: "04",
+        title: "Niwelacje terenu, skarpowanie"
+    },
+    {
+        num: "05",
+        title: "Wykopy pod sieci kanalizacyjne"
+    },
+    {
+        num: "06",
+        title: "Wykopy pod sieci energetyczne i telekomunikacyjne, fundamenty"
+    },
+]
+
 export default function Services() {
-    return <div>Usługi</div>
+    return <section className="min-h-[80vh] flex flex-col justify-center py-12 xl:py-0">
+        <div className="container mx-auto">
+            <motion.div initial={{
+                opacity: 0
+            }} animate={{
+                opacity: 1,
+                transition: { delay: 2.4, duration: 0.4, ease: "easeIn" }
+            }} className="grid gird-cols-1 md:grid-cols-2 gap-[60px]">
+                {services.map((service, index) => {
+                    return <div key={index} className="flex flex-col flex-1 justify-evenly gap-6 group hover:text-outline-hover transition-all duration-500 cursor-pointer">
+                        <div>
+                            <div className="text-5xl font-extrabold text-outline text-transparent ">
+                                {service.num}
+                            </div>
+                            <h2>{service.title}</h2>
+                        </div>
+                        <div className="border-b border-white/20 w-full"></div>
+                    </div>
+                })}
+            </motion.div>
+        </div>
+    </section>
 }
--- src/components/Stats.tsx
+++ src/components/Stats.tsx
@@ -12,23 +12,25 @@ const stats = [
         text: "Zrealizowanych projektów"
     },
     {
-        num: 500,
-        text: "Zadowolonych klientów"
-    }
+        num: 250,
+        text: "Pozytywnych opiinii"
+    },
 ]
 
 export default function Stats() {
-    return <section>
+    return <section className="pt-4 pb-12 xl:pt-0 xl:pb-0">
         <div className="container mx-auto">
-            <div className="flex flex-wrap gap-6
-            max-w-[80vh] mx-auto xl:max-w-none">
+            <div className="flex flex-col  gap-6 max-w-[80vw] mx-auto xl:max-w-none md:flex-wrap md:flex-row">
                 {stats.map((stat, index) => {
+                    console.log(stat.text.length)
                     return <div className="flex
-                    items-center justify-center flex-1 xl:justify-start" key={index}>
-                        <CountUp end={stat.num} delay={2} duration={5} className="text-4xl xl:text-6xl font font-extrabold" />
+                    items-center justify-center gap-4 flex-1 xl:justify-start " key={index}>
+                        <CountUp end={stat.num} delay={2} duration={5} className="text-4xl xl:text-6xl font-extrabold text-start" />
+                        <p className={`${stat.text.length < 15 ? "max-w-[100px]" : "max-w-[150px]"} leading-snug text-white/80 `}>{stat.text}</p>
                     </div>
+
                 })}
             </div>
         </div>
-    </section>
+    </section >
 }
