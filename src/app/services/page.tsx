58576d601ac2b55d756bbff137e508f0f9a6bd6a
TN <tn@macbook-pro-tn.home>
2024-11-12 12:14:34 -0800

    fixed styles for serrvices page

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,56 +1,61 @@
-"use client"
+"use client";
 import { BsArrowDownRight } from "react-icons/bs";
 import Link from "next/link";
-import { motion } from "framer-motion"
-const services = [
-    {
-        num: "01",
-        title: "Usuwanie korzeni"
+import { motion } from "framer-motion";
 
-    },
+const services = [
+    { title: "Usuwanie korzeni" },
+    { title: "Prace ogrodowe" },
+    { title: "Drenaże, odwodnienia" },
+    { title: "Niwelacje terenu, skarpowanie" },
+    { title: "Wykopy pod sieci kanalizacyjne" },
     {
-        num: "02",
-        title: "Prace ogrodowe"
+        title: "Wykopy pod sieci energetyczne i telekomunikacyjne, fundamenty",
     },
-    {
-        num: "03",
-        title: "Drenaże, odwodnienia"
-    },
-    {
-        num: "04",
-        title: "Niwelacje terenu, skarpowanie"
-    },
-    {
-        num: "05",
-        title: "Wykopy pod sieci kanalizacyjne"
-    },
-    {
-        num: "06",
-        title: "Wykopy pod sieci energetyczne i telekomunikacyjne, fundamenty"
-    },
-]
+];
 
 export default function Services() {
-    return <section className="min-h-[80vh] flex flex-col justify-center py-12 xl:py-0">
-        <div className="container mx-auto">
-            <motion.div initial={{
-                opacity: 0
-            }} animate={{
-                opacity: 1,
-                transition: { delay: 2.4, duration: 0.4, ease: "easeIn" }
-            }} className="grid gird-cols-1 md:grid-cols-2 gap-[60px]">
-                {services.map((service, index) => {
-                    return <div key={index} className="flex flex-col flex-1 justify-evenly gap-6 group hover:text-outline-hover transition-all duration-500 cursor-pointer">
-                        <div>
-                            <div className="text-5xl font-extrabold text-outline text-transparent ">
-                                {service.num}
+    return (
+        <section className="min-h-[80vh] flex flex-col justify-center py-12 xl:py-0">
+            <h1 className="h1 text-center mb-32">Wykonujemy:</h1>
+            <div className="container mx-auto">
+                <motion.div
+                    initial={{ opacity: 0 }}
+                    animate={{
+                        opacity: 1,
+                        transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
+                    }}
+                    className="grid grid-cols-1 gap-[60px]"
+                >
+                    {services.map((service, index) => (
+                        <motion.div
+                            key={index}
+                            initial={{ opacity: 0 }}
+                            animate={{
+                                opacity: 1,
+                                transition: { delay: index + 2, ease: "linear" },
+                            }}
+                            className="flex flex-col flex-1 justify-center items-center gap-6 group hover:text-outline-hover transition-all duration-500 cursor-pointer"
+                        >
+                            <div>
+                                <h2 className="text-xl font-bold group-hover:text-accent transition-all duration-500 uppercase">
+                                    {service.title}
+                                </h2>
                             </div>
-                            <h2>{service.title}</h2>
-                        </div>
-                        <div className="border-b border-white/20 w-full"></div>
-                    </div>
-                })}
-            </motion.div>
-        </div>
-    </section>
+                            <motion.div
+                                className="border-b border-white/20"
+                                initial={{ width: "0%" }}
+                                animate={{ width: "50%" }}
+                                transition={{
+                                    duration: 0.5,
+                                    ease: "easeOut",
+                                }}
+
+                            />
+                        </motion.div>
+                    ))}
+                </motion.div>
+            </div>
+        </section>
+    );
 }
