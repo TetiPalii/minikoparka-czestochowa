f2a7a74c6508ba075b0eec74b846dfcf4abadc14
TN <tn@macbook-pro-tn.home>
2025-06-21 04:35:12 -0700

    fixed errors

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    experimental: {
-    legacyBrowsers: false, // WYŁĄCZA IE11
-  },
-};
+const nextConfig = {}
 
 export default nextConfig;
